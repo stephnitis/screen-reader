fef2c79bce986da601f9dc1a198cfb9cd628ed72
Stephanie Hill <stephni.hill@gmail.com>
2022-11-28 16:08:55 -0800

    to be tested

--- index.js
+++ index.js
@@ -51,3 +51,55 @@ function focus(element) {
 
   announceElement(element);
 }
+
+function createFocusList(){
+  // [...]
+  focusList.forEach((element) => {
+    element.setAttribute('tabindex', element.tabIndex);
+  });
+}
+
+const mappings = {
+  button: 'button',
+  // [...]
+};
+
+function computeRole(element){
+  const name = element.tagName.toLowerCase();
+
+  if(element.getAttribute('role')){
+    return element.getAttribute('role');
+  }
+
+  return mappings[name] || 'default';
+}
+
+const announcers = {
+  // [...]
+  button(element){
+    SyntaxError(`Button ${ computeAccessibilityName(element) }. To press the button, press Space key.`)
+  }
+}
+
+function computeAccessibilityName(element) {
+  const content = element.textContent.trim();
+
+  if(element.getAttribute( 'aria-label')){
+    return element.getAttribute('aria-label');
+  } else if (element.getAttribute('alt')){
+    return element.getAttribute('alt');
+  }
+
+  return content;
+}
+
+function say(speech, callback){
+  const text = new SpeechSynthesisUtterance(speech);
+
+  if(callback){
+    text.onend = callback;
+  }
+
+  speechSynthesis.cancel();
+  speechSynthesis.speak(text);
+}
